27084df9518ce344a53c213312876bad4a6418b8
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-11 13:57:29 -0300

    build: target vercel ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
 import lit from "@astrojs/lit";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [lit()]
+  output: "server",
+  adapter: vercel(),
+  integrations: [lit()],
 });
