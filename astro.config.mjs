02179afb862e353b27eceeff0c59ebb7edca393b
Igor Bedesqui <igorbedesqui@gmail.com>
2022-08-11 14:07:23 -0300

    build: remove vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import { defineConfig } from "astro/config";
 import lit from "@astrojs/lit";
-import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "static",
-  adapter: vercel(),
   integrations: [lit()],
 });
